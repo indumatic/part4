eab190ce2f9923cf175586aeddfafe3bde0f02fb
indumatic <electrico@indumatic.net>
2023-12-01 14:13:18 +0100

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 const requesLogger = (request, response, next) => {
     console.log('Method:', request.method)
